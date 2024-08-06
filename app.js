0c3d0700c489f213def121319120ac7e4f381b1f
sarkis <sarkis.gmail.com>
2024-08-06 13:29:02 +0300

    read

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 var cors = require('cors')
 const xlsx=require('xlsx');
 let wb= xlsx.readFile('data.xlsx');
-let ws= wb.sheets['names'];
+let ws= wb.Sheets['names'];
 let _data = xlsx.utils.sheet_to_json(ws)
  
 app.use(cors())
@@ -17,12 +17,12 @@ app.get('/', function (req, res) {
   })
 
   app.get('/students', function (req, res) {
-    var data=[
-        {id:1, name: 'sako'},
-        {id:2, name: 'sarkis'},
-        {id:3, name: 'fadlallah'}
-    ]
-    res.send(data)
+    // var data=[
+    //     {id:1, name: 'sako'},
+    //     {id:2, name: 'sarkis'},
+    //     {id:3, name: 'fadlallah'}
+    // ]
+    res.send(_data)
   })
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
